ac2a0549946728ad7cf72d74e3dec35426611c99
Nazarkep355 <61923362+Nazarkep355@users.noreply.github.com>
2023-02-17 22:33:09 +0200

    header.html creation 1

--- src/main/java/com/example/animelist/config/UserConfig.java
+++ src/main/java/com/example/animelist/config/UserConfig.java
@@ -3,13 +3,19 @@ package com.example.animelist.config;
 import com.example.animelist.repos.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
 @Configuration
 @RequiredArgsConstructor
@@ -29,4 +35,5 @@ public class UserConfig {
         authenticationProvider.setPasswordEncoder(passwordEncoder);
         return authenticationProvider;
     }
+
 }
